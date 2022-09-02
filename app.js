04c7ef51db47c69e37ec7e792c44fbc3c9e97a1d
ry6n1k <levchenkoyarya@gmail.com>
2022-09-02 20:16:50 +0300

    cyclic crud

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const db = require("cyclic-dynamodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  console.log("just got a request");
-  res.send("Deploy Cyclic App");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// create an item
+app.post("/:col/:key", async (req, res) => {
+  const col = req.params.col;
+  const key = req.params.key;
+  const item = await db.collection(col).set(key, req.body);
+  console.log(JSON.stringify(item, null, 2));
+  res.json(item).end();
+});
+
+// get a full
+app.get("/:col", async (req, res) => {
+  const col = req.params.col;
+  const items = await db.collection(col).list();
+  console.log(JSON.stringify(items, null, 2));
+  res.json(items).end();
 });
 
 app.listen(port);
