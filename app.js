56613879a6d036f8337202216d02400bea6590c7
ry6n1k <levchenkoyarya@gmail.com>
2022-09-02 19:17:04 +0300

    deploy cyclic 2

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const express = require("express");
-const CyclicDb = require("cyclic-dynamodb");
-
 const app = express();
-const db = CyclicDb("beautiful-cow-sweatsuitCyclicDB");
 const port = process.env.PORT || 3000;
 
-app.set("view engine", "hbs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.get("/", async (req, res) => {
-  let animals = db.collection("animals");
-  let leo = await animals.set("leo", {
-    type: "cat",
-    color: "orange",
-  });
-  let item = await animals.get("leo");
-  console.log(item)
-  res.send('Deploy Cyclic App');
+app.get("/", (req, res) => {
+  console.log("just got a request");
+  res.send("Deploy Cyclic App");
 });
 
 app.listen(port);
