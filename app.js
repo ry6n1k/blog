51c8f142513fec34a7349b7dd66044f8c6048460
ry6n1k <levchenkoyarya@gmail.com>
2022-09-08 19:43:09 +0300

    reset projects

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 const express = require("express");
-const db = require("cyclic-dynamodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// create an item
-app.post("/:col/:key", async (req, res) => {
-  const col = req.params.col;
-  const key = req.params.key;
-  const item = await db.collection(col).set(key, req.body);
-  console.log(JSON.stringify(item, null, 2));
-  res.json(item).end();
+app.get("/", (request, response) => {
+  response.send("hello world rybnik");
 });
 
-// get a full
-app.get("/:col", async (req, res) => {
-  const col = req.params.col;
-  const items = await db.collection(col).list();
-  console.log(JSON.stringify(items, null, 2));
-  res.json(items).end();
+app.listen(port, () => {
+  console.log("start server ...");
 });
-
-app.listen(port);
--- readme.md
+++ readme.md
@@ -1,13 +1,1 @@
 # blog example
-
-## practice typescript-express
-
-`npm run start` - запустить tsс
-
-`npm run test` - запустить тесты
-
-### docker
-
-`docker-compose build` - сборка Docker
-
-`docker-compose up` - запустить Docker
